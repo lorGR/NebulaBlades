bb7b75e5fcc6103d8fa90f84d7397dc4dc39cf97
karearl <69432923+karearl@users.noreply.github.com>
2024-06-10 09:29:53 +0300

    add player animations back
    added hurt and run animations (needs to be implemented)
    things take damage and die now :skull:

--- scenes/npcs/slime.gd
+++ scenes/npcs/slime.gd
@@ -1,23 +1,34 @@
 extends Node2D
 
-@export var speed: float = 100.0
+@export var SPEED: float = 35.0
+@export var HEALTH: float = 10.0
+@export var DAMAGE: float = 1.0
+@onready var animated_sprite_2d = $AnimatedSprite2D
 var player: Node2D
 
 func _ready():
-	$Area2D.area_entered.connect(_on_area_2d_area_entered)
 	print("slime spawned")
 
 	# Find the player in the scene tree (assuming the player node is a sibling to the enemy node)
 	player = get_parent().get_node("Player")
 
 func _process(delta: float):
+	if HEALTH <= 0:
+		animated_sprite_2d.play("die")
+		queue_free()
+	
 	if player:
 		# Calculate the direction to the player
 		var direction = (player.global_position - global_position).normalized()
 		# Move the enemy towards the player
-		position += direction * speed * delta
+		position += direction * SPEED * delta
+		
 
 func _on_area_2d_area_entered(area):
-	if area.is_in_group("player_projectile"):
+	if area.is_in_group("player"):
 		print("*takes damage* or something >_>")
-		# Add logic for taking damage or other actions
+		melee(player)
+
+func melee(body: Node2D):
+	body.HEALTH -= DAMAGE
+	print("dealt " + str(DAMAGE) + " to " + body.name)
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
-const SPEED = 100.0
+@export var SPEED: float = 75.0
+@export var HEALTH: float = 35.0
+@export var DAMAGE: float = 2
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
@@ -12,15 +15,32 @@ func _physics_process(delta):
 		position.y += up * SPEED * delta
 
 	if direction != 0:
-		position.x += direction * SPEED * delta
+		position.x += direction * SPEED * delta		
 
 func _ready():
-	$Area2D.area_entered.connect(_on_area_2d_area_entered)
+	pass
+	
+func _process(delta):
+	if HEALTH <= 0:
+		animated_sprite_2d.play("die")
+		queue_free()
 
 func _on_area_2d_area_entered(area):
 	if area.is_in_group("enemy"):
+		var slime = area.get_parent()
 		print("An enemy touched me Q_Q")
+		melee(slime)
 		# Take damage or die or whatever
 		
 	elif area.is_in_group("collectible"):
 		print("Collected something")
+
+func melee(body: Node2D):
+	body.HEALTH -= DAMAGE
+	print("dealt " + str(DAMAGE) + " to " + body.name)
+	
+
+func _on_animated_sprite_2d_animation_finished():
+	if animated_sprite_2d.animation == "hurt":
+			animated_sprite_2d.play("idle")
+
